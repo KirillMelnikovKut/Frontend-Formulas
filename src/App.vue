c7b566f1ff3118bfbe4fd1d3be2e9b03c933b107
astdosha <146178521+astdosha@users.noreply.github.com>
2024-10-05 21:52:18 +0300

    add auth

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@
 <script setup lang="ts">
 import UIButton from '@/components/UI/UIButton.vue';
 import UIHeader from '@/components/UI/UIHeader.vue';
+import $router from '@/router/index.ts'
+
+const isAuth = localStorage.getItem("isAuth") === "true";
+
+if (!isAuth) $router.push({ path: 'auth' });
 </script>
 
 <style scoped>
