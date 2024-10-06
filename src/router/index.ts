516b4528f448f1f6d5d2f1ff16e6eb351f6af975
Kirill Melnikov <k.melnikov@aoneiro.com>
2024-10-06 13:26:23 +0300

    fix

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,18 +4,61 @@ import Register from '@/pages/register.vue'
 import Test from '@/pages/test.vue'
 import Selections from '@/pages/sections/index.vue'
 import Selection from '@/pages/sections/_id.vue'
+import { LOCAL_STORAGE_KEYS } from '@/utils/constants/localStorage';
+import { useLocalStorage } from '@vueuse/core';
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [
-		{ path: '/', component: Index },
-		{ path: '/register', component: Register },
-		{ path: '/auth', component: Auth },
-		{ path: '/test', component: Test },
-		{ path: '/sections', component: Selections },
-		{ path: '/sections/:id', component: Selection },
+		{
+			path: '/',
+			meta: { requiresAuth: false },
+			component: Index
+		},
+		{
+			path: '/register',
+			meta: { requiresAuth: false },
+			component: Register
+		},
+		{
+			path: '/auth',
+			meta: { requiresAuth: false },
+			component: Auth
+		},
+		{
+			path: '/test',
+			meta: { requiresAuth: false },
+			component: Test
+		},
+		{
+			path: '/sections',
+			meta: { requiresAuth: false },
+			component: Selections
+		},
+		{
+			path: '/sections/:id',
+			meta: { requiresAuth: false },
+			component: Selection
+		},
 	]
 })
+router.beforeEach((to, _, next) => {
+	
+	const authenticated = useLocalStorage<boolean>(
+		LOCAL_STORAGE_KEYS['authenticated'],
+		false,
+	);
+	
+	if (to.name === 'Auth') {
+		authenticated.value = false;
+	}
+	
+	if (to.meta.requiresAuth && !authenticated.value) {
+		return next('/auth');
+	}
+	
+	return next();
+});
 
-export default router
+export default router;
