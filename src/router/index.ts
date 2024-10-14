2facd70a3c9532537f1e5dd8dca919a9465782e5
Kirill Melnikov <k.melnikov@aoneiro.com>
2024-10-14 11:58:45 +0300

    fix merge

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,11 @@
 import Auth from '@/pages/auth.vue';
 import Index from '@/pages/index.vue';
 import Register from '@/pages/register.vue';
-import Tests from '@/pages/tests/index.vue'
+import Tests from '@/pages/tests/index.vue';
 import Test from '@/pages/tests/_id.vue';
 import Selections from '@/pages/sections/index.vue';
 import Selection from '@/pages/sections/_id.vue';
-import { LOCAL_STORAGE_KEYS } from '@/utils/constants/localStorage';
-import { useLocalStorage } from '@vueuse/core';
+import theory from '@/pages/theory.vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
@@ -22,16 +21,19 @@ const routes = [
   },
   {
     path: '/register',
-    meta: { requiresAuth: true },
     component: Register,
   },
   {
     path: '/auth',
-    meta: { requiresAuth: true },
     component: Auth,
   },
   {
     path: '/test',
+    meta: { requiresAuth: true },
+    component: Tests,
+  },
+  {
+    path: '/test/:id',
     meta: { requiresAuth: true },
     component: Test,
   },
@@ -49,62 +51,21 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      meta: { requiresAuth: true },
-      component: Index,
-    },
-    {
-      path: '/register',
-      meta: { requiresAuth: true },
-      component: Register,
-    },
-    {
-      path: '/auth',
-      meta: { requiresAuth: true },
-      component: Auth,
-    },     
-    {
-      path: '/test',
-      meta: { requiresAuth: true },
-      component: Tests,
-    },
-    {
-      path: '/test/:id',
-      meta: { requiresAuth: true },
-      component: Test,
-    },
-    {
-      path: '/sections',
-      meta: { requiresAuth: true },
-      component: Selections,
-    },
-    {
-      path: '/sections/:id',
-      meta: { requiresAuth: true },
-      component: Selection,
-    },
-  ],
-
-
-
+  routes,
+});
 
 router.beforeEach((to, from, next) => {
   const authenticated = { value: true };
-
+  
   if (to.meta.requiresAuth && !authenticated.value) {
-    if (to.path !== '/auth') {
-      return next('/auth');
-    }
+    return next('/auth');
   }
-
+  
   if (to.path === '/auth' && authenticated.value) {
     return next('/');
   }
-
+  
   next();
 });
-
 
 export default router;
